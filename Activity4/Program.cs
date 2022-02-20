6959e60fbc70d3f7e86e05c294165e93f5f28adb
ETrsw <78511310+ETrsw@users.noreply.github.com>
2022-02-19 20:42:19 -0500

    Add project files.

---
+++ Activity4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+namespace ThisAPI
+{
+    interface registeruser
+    {
+        string Username { get; set; }
+        string Password { get; set; }
+        int Age { get; set; }
+        string Email { get; set; }
+         
+       
+        
+    }
+   
+    public class User: registeruser
+    {
+
+        private string username;
+        private string password;
+        private int age;    
+        private string email;   
+
+        public string Username { get { return username; } set { username = value; } }
+        public string Password { get { return password; } set { password = value; } }
+        public int Age { get { return age;} set { age = value; } }
+        public string Email { get { return email; } set { email = value; } }     
+    }
+
+    
+}
+
+public class RegistrationService
+{
+    public delegate void RegistrationServiceDelegate(object user, EventArgs eventArgs);
+    public event RegistrationServiceDelegate RegistrationSuccessful;
+    public void RegisterUser(ThisAPI.User user)
+    {
+        Console.WriteLine("Your account is being set up.");
+        Thread.Sleep(100);
+
+        OnRegistrationSuccess();
+    }
+    protected virtual void OnRegistrationSuccess()
+    {
+        if (RegistrationSuccessful != null)
+            RegistrationSuccessful(this, null);
+    }
+}
+
+
+public class AppService
+{
+    public void OnRegistrationSuccess(object user,EventArgs eventArgs)
+    {
+        Console.WriteLine("Registration successful.");
+    }
+}
+class Program
+{
+    static void Main ()
+    {
+        var newUser = new ThisAPI.User
+        {
+            Username = "blablabla",
+            Password = "blablabla1",
+            Email = "blablabla@dgd.com",
+            Age = 90
+        };
+       var registrationservice = new RegistrationService();
+       var appservice = new AppService();
+       registrationservice.RegistrationSuccessful += appservice.OnRegistrationSuccess;
+       registrationservice.RegisterUser(newUser);
+        Console.ReadKey();
+    }
+}
